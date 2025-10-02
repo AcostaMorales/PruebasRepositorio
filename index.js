62fe5eccb21ee77fabc37b65adf726dd72d44e47
Pablo Daniel Acosta Morales <acostamoralespablodaniel@gmail.com>
2025-10-02 14:29:40 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//Declarar variables en JavaScript
+//const -> variable que no cambia
+//let -> variable que puede cambiar
+//var -> variable que puede cambiar (no se usa mucho)
+
+const http =  require('http');
+
+function requestController() {
+    //logica de nuestra funcion
+    console.log("Recibimos una nueva peticion");
+
+}
+
+//Configuracion del servidor
+const server = http.createServer(requestController);
+
+server.listen(3000);
